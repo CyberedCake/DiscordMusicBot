7fd0a2fd307f466c5fe45a9653ec09e3afe7a271
CyberedCake <61170080+CyberedCake@users.noreply.github.com>
2023-07-24 14:41:40 -0400

    Make stop permission only (admin command)

--- src/main/java/net/cybercake/discordmusicbot/commands/list/admin/Stop.java
+++ src/main/java/net/cybercake/discordmusicbot/commands/list/admin/Stop.java
@@ -1,12 +1,14 @@
-package net.cybercake.discordmusicbot.commands.list;
+package net.cybercake.discordmusicbot.commands.list.admin;
 
 import net.cybercake.discordmusicbot.Embeds;
 import net.cybercake.discordmusicbot.PresetExceptions;
 import net.cybercake.discordmusicbot.commands.Command;
 import net.cybercake.discordmusicbot.queue.Queue;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
 
 import java.awt.*;
 import java.util.Date;
@@ -18,6 +20,9 @@ public class Stop extends Command {
                 "stop", "Removes the bot from the voice chat."
         );
         this.aliases = new String[]{"disconnect", "leave", "remove", "goodbye"};
+        this.permission = DefaultMemberPermissions.enabledFor( // need these permissions to execute
+                Permission.MANAGE_CHANNEL, Permission.MANAGE_SERVER, Permission.ADMINISTRATOR, Permission.VOICE_MOVE_OTHERS, Permission.VOICE_MUTE_OTHERS
+        );
     }
 
     @Override
@@ -29,7 +34,7 @@ public class Stop extends Command {
         Queue queue = PresetExceptions.trackIsNotPlaying(event, event.getMember(), true);
         if(queue == null) return;
 
-        if(member.getVoiceState() == null || member.getVoiceState().getChannel() == null || !member.getVoiceState().getChannel().asVoiceChannel().equals(queue.getVoiceChannel())) {
+        if(member.getVoiceState() == null || member.getVoiceState().getChannel() == null || !member.getVoiceState().getChannel().equals(queue.getVoiceChannel())) {
             Embeds.throwError(event, member.getUser(), "You must be in the voice chat to skip a song", true, null); return;
         }
 
